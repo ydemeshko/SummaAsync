98568a63471cd1cff247135b1c5c184558fdd300
Yana <joanna27@mail.ru>
2019-11-11 13:32:02 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+//using System.Threading.Tasks;
+
+
+namespace Summa_Async
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        static void Summa(int n, CancellationToken token)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Операция прервана токеном");
+                    return;
+                }
+                result += i;
+                Console.WriteLine($"Сумма {i} чисел  равна {result}");
+                Thread.Sleep(1000);
+            }
+        }
+        // определение асинхронного метода
+        static async void SummaAsync(int n, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+                return;
+            await Task.Run(() => Summa(n, token));
+        }
+
+        static void Main(string[] args)
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+            CancellationToken token = cts.Token;
+            Console.WriteLine("Введите количество чисел: ");
+            Console.ReadLine(int m);
+            SummaAsync(m, token);
+            Thread.Sleep(3000);
+            cts.Cancel();
+            Console.Read();
+        }
+    }
+}
